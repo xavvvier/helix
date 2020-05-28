5a8af4417bfbc4a0a2003d6f5be19e3c81a0ef64
xavvvier <xavvvier@users.noreply.github.com>
2020-05-27 21:14:15 -0300

    initial phoenix project

--- config/config.exs
+++ config/config.exs
@@ -11,6 +11,22 @@ import Config
 
 config :builder, ecto_repos: [Helix.Builder.Repo]
 
+# Configures the endpoint
+config :console, ConsoleWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "X5ZHo5xYGew2kz00Jz3j8f1yJ41+9O7E2GDmfJGVD/nZ0/cGgOE4qui4v7CDuWAv",
+  render_errors: [view: ConsoleWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Console.PubSub,
+  live_view: [signing_salt: "J+5V5zGd"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -6,3 +6,12 @@ config :builder, Helix.Builder.Repo,
   password: "",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :console, ConsoleWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
